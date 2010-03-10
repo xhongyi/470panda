778991126377bc9255198ea77a94a3f207e0d3ac
jeffxanthus <jeffxanthus@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-03-10 05:59:30 +0000

    rob now making sense

--- trunk/verilog/rob.v
+++ trunk/verilog/rob.v
@@ -11,6 +11,7 @@ module decoder( //inputs
 				dest,
 				opa,
 				opb,
+				dest_valid,
 				opa_valid,
 				opb_valid,
 				immr
@@ -21,6 +22,7 @@ output [5:0] opcode;
 output [4:0] dest;
 output [4:0] opa;
 output [4:0] opb;
+output		 dest_valid;
 output 		 opa_valid;
 output		 opb_valid;
 output [20:0] immr;
@@ -30,6 +32,7 @@ reg [5:0] opcode;
 reg [4:0] opa;
 reg [4:0] opb;
 reg [20:0] immr;
+reg	       dest_valid;
 reg 	   opa_valid;
 reg 	   opb_valid;
 
@@ -41,6 +44,7 @@ begin
 	3'h2:
 	begin
 		dest = instr[4:0];
+		dest_valid = 1'b1;
 		opa = instr[25:21];
 		opa_valid = 1;
 		opb = instr[20:16];
@@ -50,6 +54,7 @@ begin
 	3'h3:
 	begin
 		dest = instr[25:21];
+		dest_valid = 1'b1;
 		opa = 5'b11111;
 		opa_valid = 1'h0;
 		opb = instr[20:16];
@@ -58,16 +63,18 @@ begin
 	end
 	3'h1, 3'h4, 3'h5:
 	begin
-		dest = instr[25:21];
-		opa = 5'b11111;
-		opa_valid = 1'b0;
+		dest = (instr[31:26] ==`STQ_INST)? 5'h1f : instr[25:21];
+		dest_valid = (instr[31:26] ==`STQ_INST)?1'b0 : 1'b1;
+		opa = (instr[31:26] ==`STQ_INST)?instr[25:21]:5'b11111;
+		opa_valid = (instr[31:26] ==`STQ_INST)?1'b1:1'b0;
 		opb = instr[20:16];
 		opb_valid = 1'h1;
-		immr = {5'b0,instr[15:0]};
+		immr = 21'h1fff;//{5'b0,instr[15:0]};
 	end
 	3'h6, 3'h7:
 	begin
 		dest = instr[25:21];
+		dest_valid = 1'b1;
 		opa = 5'b11111;
 		opa_valid = 1'b0;
 		opb = 5'b11111;
@@ -80,6 +87,7 @@ begin
 		opa = 5'b11111;
 		opb = 5'b11111;
 		immr = 21'h1ffff;
+		dest_valid = 1'b0;
 		opa_valid = 1'b0;
 		opb_valid = 1'b0;
 	end
@@ -123,12 +131,14 @@ module rob(//inputs
 			//fetch
 			tail_pc,
 			//dispatch
-			mt_ar_a,
-			mt_ar_b,
+			rs_mt_ar_a,
+			rs_mt_ar_b,
 			mt_ar_a1,
 			mt_ar_b1,
 			mt_ar_a2,
 			mt_ar_b2,
+			rs_mt_ar_a_valid,
+			rs_mt_ar_b_valid,
 			mt_ar_a1_valid,
 			mt_ar_a2_valid,
 			mt_ar_b1_valid,
@@ -171,12 +181,14 @@ input [6:0] cdb_pr_tag_3;  //Note: the bandwidth is CDB_WIDTH;
 
 output [63:0] tail_pc;
 
-output [4:0] mt_ar_a;
-output [4:0] mt_ar_b;
+output [4:0] rs_mt_ar_a;
+output [4:0] rs_mt_ar_b;
 output [4:0] mt_ar_a1;
 output [4:0] mt_ar_b1;
 output [4:0] mt_ar_a2;
 output [4:0] mt_ar_b2;
+output		 rs_mt_ar_a_valid;
+output	     rs_mt_ar_b_valid;
 output 		 mt_ar_a1_valid;
 output		 mt_ar_b1_valid;
 output 		 mt_ar_a2_valid;
@@ -214,11 +226,51 @@ reg [5:0] tail ; //tail position, anywhere between 0 and 63.
 reg [5:0] fetch_tail;//fetch tail position, different from dispatch tail
 reg [63:0] tail_pc;
 
+
+reg [5:0]  rs_opcode0;
+reg [5:0]  rs_opcode1;
+reg [4:0]  rs_mt_ar_a;
+reg [4:0]  rs_mt_ar_b;
+reg [4:0]  mt_ar_a1;
+reg [4:0]  mt_ar_b1;
+reg [4:0]  mt_ar_a2;
+reg [4:0]  mt_ar_b2;
+reg		   rs_mt_ar_a_valid;
+reg        rs_mt_ar_b_valid;
+reg 	   mt_ar_a1_valid;
+reg		   mt_ar_b1_valid;
+reg		   mt_ar_a2_valid;
+reg		   mt_ar_b2_valid;
+reg [20:0] rs_immediate0;
+reg [20:0] rs_immediate1;
+
+
+
+
+wire [5:0]  next_rs_opcode0;
+wire [5:0]  next_rs_opcode1;
+wire [4:0]  next_rs_mt_ar_a;
+wire [4:0]  next_rs_mt_ar_b;
+wire [4:0]  next_mt_ar_a1;
+wire [4:0]  next_mt_ar_b1;
+wire [4:0]  next_mt_ar_a2;
+wire [4:0]  next_mt_ar_b2;
+wire		   next_rs_mt_ar_a_valid;
+wire 		   next_rs_mt_ar_b_valid;
+wire 	   	   next_mt_ar_a1_valid;
+wire		   next_mt_ar_b1_valid;
+wire		   next_mt_ar_a2_valid;
+wire		   next_mt_ar_b2_valid;
+wire [20:0] next_rs_immediate0;
+wire [20:0] next_rs_immediate1;
+reg  [1:0]  next_rs_mt_fl_dispatch_num;
+
+
 //Update State
-reg [31:0] next_inst[63:0];
-reg [63:0] next_pc  [63:0];
+reg [31:0] next_inst [63:0];
+reg [63:0] next_pc [63:0];
 reg [63:0] next_ready;
-reg [6:0] next_tag [63:0];
+reg [6:0] next_tag  [63:0];
 reg [6:0] next_told [63:0];
 reg [5:0] next_head;
 reg [5:0] next_tail;
@@ -227,33 +279,35 @@ reg [63:0] next_tail_pc;
 decoder decoder_1 (
 					
 					.instr(inst0),
-					.opcode(rs_opcode0),
-					.dest(mt_ar_a),
-					.opa(mt_ar_a1),
-					.opb(mt_ar_a2),
-					.opa_valid(mt_ar_a1_valid),
-					.opb_valid(mt_ar_a2_valid),
-					.immr(rs_immediate0)
+					.opcode(next_rs_opcode0),
+					.dest(next_rs_mt_ar_a),
+					.opa(next_mt_ar_a1),
+					.opb(next_mt_ar_a2),
+					.dest_valid(next_rs_mt_ar_a_valid),
+					.opa_valid(next_mt_ar_a1_valid),
+					.opb_valid(next_mt_ar_a2_valid),
+					.immr(next_rs_immediate0)
 				);
 
 				
 decoder decoder_2 (
 					
 					.instr(inst1),
-					.opcode(rs_opcode1),
-					.dest(mt_ar_b),
-					.opa(mt_ar_b1),
-					.opb(mt_ar_b2),
-					.opa_valid(mt_ar_b1_valid),
-					.opb_valid(mt_ar_b2_valid),
-					.immr(rs_immediate1)
+					.opcode(next_rs_opcode1),
+					.dest(next_rs_mt_ar_b),
+					.opa(next_mt_ar_b1),
+					.opb(next_mt_ar_b2),
+					.dest_valid(next_rs_mt_ar_b_valid),
+					.opa_valid(next_mt_ar_b1_valid),
+					.opb_valid(next_mt_ar_b2_valid),
+					.immr(next_rs_immediate1)
 				);
 					
 
 
 
 
-integer index, jindex;
+integer index, jindex, kindex;
 
 assign full = ((fetch_tail==6'd63 && head==6'd0)|(head==fetch_tail+6'd1));
 assign almost_full = ((fetch_tail==6'd62&&head==6'd0)&&(fetch_tail==6'd63&&head==6'd1)&&(head==fetch_tail+6'd2));
@@ -265,7 +319,15 @@ assign  tail_plus_two = (tail == 6'd62) ? 6'd0 :  (tail == 6'd63) ? 6'd1 : tail 
 
 always @*
 begin
-
+//Load Internal State
+for (kindex = 0; kindex < 64; kindex = kindex + 1)
+begin
+	next_inst[kindex] = inst[kindex];
+	next_pc[kindex] = pc[kindex];
+	next_tag[kindex] = tag[kindex];
+	next_told[kindex] = told[kindex];
+	next_ready[kindex] = ready[kindex];
+end
 //	if (reset)
 //else
     //fetch
@@ -280,7 +342,7 @@ begin
 	end
 	else if (almost_full)
 	begin
-	
+		next_tail_pc = pc0;
 	    if(fetch_tail == 6'd63)
 			begin
 				next_inst[0] = inst0;
@@ -294,11 +356,12 @@ begin
 	    	next_pc  [fetch_tail+6'd1] = pc0;
 			next_fetch_tail = fetch_tail + 6'd1;
 		end
-	next_tail_pc = pc0;
+	
 	end//of almost full
 
 	else
 	begin
+		next_tail_pc = pc1;
 		if(fetch_tail == 6'd62)
 		begin
 			next_inst[63] = inst0;
@@ -324,7 +387,7 @@ begin
 			next_fetch_tail = fetch_tail + 6'd2;
 		
 		end
-	next_tail_pc = pc1;
+	
     end //of else (far from full)
 	//end of fetch
 	//Start of dispatch	
@@ -334,32 +397,33 @@ begin
 	case (rs_avail) 
 		2'b00,2'b11:
 		begin
-			
-			rs_mt_fl_dispatch_num = 2'b0;
+		$display("did not dispatch");	
+			next_rs_mt_fl_dispatch_num = 2'b0;
  			next_tail = tail;
 		end
 		2'b01:
 		begin
-			
+			$display("dispatched one");
 			next_tag[tail] = fl_pr0;
 			next_told[tail] = mt_p0told;
-			rs_mt_fl_dispatch_num = 2'b1;
+			next_rs_mt_fl_dispatch_num = 2'b1;
 			next_tail = (tail == 6'd63)? 6'd0 : tail + 6'b1;
 		end
 		2'b10:
 		begin
-			
+			$display("dispatched two");
 			next_tag[tail] = fl_pr0;
 			if (tail == 63) next_tag[0] = fl_pr1;
-			else next_tag[tail+6'd1] = fl_pr1;
+			else next_tag[tail+1] = fl_pr1;
 			next_told [tail] = mt_p0told;
 			if (tail == 63) next_told [0] = mt_p1told;
-			else next_told[tail + 6'd1] = mt_p1told;
-		    rs_mt_fl_dispatch_num = 2'b10;
+			else next_told[tail + 1] = mt_p1told;
+		    next_rs_mt_fl_dispatch_num = 2'b10;
 			next_tail = (tail == 6'd62) ? 6'd0 : (tail == 6'd63) ? 6'd1 : tail + 6'd2;
 		end
 	endcase
-	$display("head is: %h tail is %h fetch tail is: %h",head, tail, fetch_tail);
+	$display("head is: %h tail is %h fetch tail is: %h tag[head]: %h tag[tail]: %h ready[head]: ",head, tail, fetch_tail, tag[head], tag[tail], ready[head]);
+	$display("next_head is: %h next_tail is: %h next_tag[head] is: %h, next_tag[tail] is: %h",next_head, next_tail, next_tag[head], next_tag[tail]);
 	//end of dispatch
 	//complete
 	//checklist:
@@ -417,6 +481,7 @@ begin
 		next_head = head + 1;
 		next_ready[head] = 0;
 		fl_retire_tag_a	= tag[head];
+		fl_retire_tag_b = 7'h7f;
 		fl_retire_num = 2'b01;
 	end
 	else
@@ -424,6 +489,8 @@ begin
 		//do nothing
 		next_head = head;
 		fl_retire_num = 2'b00;
+		fl_retire_tag_a = 7'h7f;
+		fl_retire_tag_b = 7'h7f;
 	end
 	
 
@@ -437,22 +504,66 @@ begin //of sequential logic
 		 tail <= `SD 64'h0;
 		 fetch_tail <= `SD 64'h0;
 		 tail_pc <= `SD 64'b0;
+		 rs_mt_ar_a <= `SD 5'b0;
+		 rs_mt_ar_b <= `SD 5'b0;
+		 mt_ar_a1 <= `SD 5'b0;
+		 mt_ar_b1 <= `SD 5'b0;
+		 mt_ar_a2 <= `SD 5'b0;
+		 mt_ar_b2 <= `SD 5'b0;
+		 rs_mt_ar_a_valid <= `SD 1'b0;
+		 rs_mt_ar_b_valid <= `SD 1'b0;
+		 mt_ar_a1_valid <= `SD 1'b0;
+		 mt_ar_b1_valid <= `SD 1'b0;
+		 mt_ar_a2_valid <= `SD 1'b0;
+		 mt_ar_b2_valid <= `SD 1'b0;
+		 rs_opcode0 <= `SD 6'b0;
+		 rs_opcode1 <= `SD 6'b0;
+		 rs_immediate0 <= `SD 21'b0;
+		 rs_immediate1 <= `SD 21'b0;
+		 rs_mt_fl_dispatch_num <= `SD 2'b0;
+		 
+		for (index = 0; index < 64; index = index + 1)
+		begin
+			inst[index] <= `SD 32'h0;
+			pc[index] <= `SD 64'h0;
+			tag[index] <= `SD 7'h0;
+			told[index] <= `SD 7'h0;
+			ready[index] <= `SD 1'h0;
+		end
 		//initialization
 	end
 	else
 	begin
+			for (kindex = 0; kindex < 64; kindex = kindex + 1)
+			begin
+				inst[kindex] <= `SD next_inst[kindex];
+				pc[kindex] <= `SD next_pc[kindex];
+				tag[kindex] <= `SD next_tag[kindex];
+				told[kindex] <= `SD next_told[kindex];
+				ready[kindex] <= `SD next_ready[kindex];
+			end
 			tail_pc <= `SD next_tail_pc;
 			fetch_tail <= `SD next_fetch_tail;
 			head <= `SD next_head;
 			tail <= `SD next_tail;
-		for (index = 0; index < 64; index = index + 1)
-		begin
-			inst[index] <= `SD next_inst[index];
-			pc[index] <= `SD next_pc[index];
-			tag[index] <= `SD next_tag[index];
-			told[index] <= `SD next_told[index];
-			ready[index] <= `SD next_ready[index];
-		end
+			rs_mt_ar_a <= `SD next_rs_mt_ar_a;
+		 	rs_mt_ar_b <= `SD next_rs_mt_ar_b;
+		 	mt_ar_a1 <= `SD next_mt_ar_a1;
+		 	mt_ar_b1 <= `SD next_mt_ar_b1;
+		 	mt_ar_a2 <= `SD next_mt_ar_a2;
+		 	mt_ar_b2 <= `SD next_mt_ar_b2;
+			 rs_mt_ar_a_valid <= `SD next_rs_mt_ar_a_valid;
+			 rs_mt_ar_b_valid <= `SD next_rs_mt_ar_b_valid;
+			 mt_ar_a1_valid <= `SD next_mt_ar_a1_valid;
+			 mt_ar_b1_valid <= `SD next_mt_ar_b1_valid;
+			 mt_ar_a2_valid <= `SD next_mt_ar_a2_valid;
+			 mt_ar_b2_valid <= `SD next_mt_ar_b2_valid;
+			 rs_immediate0 <= `SD next_rs_immediate0;
+			 rs_immediate1 <= `SD next_rs_immediate1;
+			 rs_opcode0 <= `SD next_rs_opcode0;
+			 rs_opcode1 <= `SD next_rs_opcode1;
+			 rs_mt_fl_dispatch_num <= `SD next_rs_mt_fl_dispatch_num;
+		
 	end
 end//of sequential logic
 
