9336f44953e6a1f246591e02d7664816ce7c575e
jeffxanthus <jeffxanthus@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-03-11 03:48:30 +0000

    testcase 1 finish

--- testbench/mt_test_1.v
+++ testbench/mt_test_1.v
@@ -118,9 +118,10 @@ module mt_test;
 	// Compare the results with the correct ones
 	always @(negedge clock)
 	begin
+	#(`VERILOG_CLOCK_PERIOD/4.0);
 		$monitor("Time: %4.0f\n\nInput\n reset: %b\n rob_dispatch_num: %b\n fl_pr0: %b\n fl_pr1: %b\n rob_ar_a_valid: %b\n rob_ar_a1_valid: %b\n rob_ar_a2_valid: %b\n rob_ar_b_valid: %b\n rob_ar_b1_valid: %b\n rob_ar_b2_valid: %b\n rob_ar_a: %b\n rob_ar_a1: %b\n rob_ar_a2: %b\n rob_ar_b: %b\n rob_ar_b1: %b\n rob_ar_b2: %b\n cdb_broadcast: %b\n cdb_pr_tags[0]: %b\n cdb_pr_tags[1]: %b\n cdb_pr_tags[2]: %b\n cdb_pr_tags[3]: %b\n cdb_ar_tags[0]: %b\n cdb_ar_tags[1]: %b\n cdb_ar_tags[2]: %b\n cdb_ar_tags[3]: %b\n\nOutput\n rs_pr_a1_ready: %b\n rs_pr_a1: %b\n rs_pr_a2_ready: %b\n rs_pr_a2: %b\n rs_pr_b1_ready: %b\n rs_pr_b1: %b\n rs_pr_b2_ready: %b\n rs_pr_b2: %b\n rob_p0told: %b\n rob_p1told: %b\n\n", $time, reset, rob_dispatch_num, fl_pr0, fl_pr1, rob_ar_a_valid, rob_ar_a1_valid, rob_ar_a2_valid, rob_ar_b_valid, rob_ar_b1_valid, rob_ar_b2_valid, rob_ar_a, rob_ar_a1, rob_ar_a2, rob_ar_b, rob_ar_b1, rob_ar_b2, cdb_broadcast, cdb_pr_tags[0], cdb_pr_tags[1], cdb_pr_tags[2], cdb_pr_tags[3], cdb_ar_tags[0], cdb_ar_tags[1], cdb_ar_tags[2], cdb_ar_tags[3], rs_pr_a1_ready, rs_pr_a1, rs_pr_a2_ready, rs_pr_a2, rs_pr_b1_ready, rs_pr_b1, rs_pr_b2_ready, rs_pr_b2, rob_p0told, rob_p1told);
 
-		#(`VERILOG_CLOCK_PERIOD/4.0);
+		
 
 		
 
@@ -210,7 +211,7 @@ module mt_test;
 		cr_rs_pr_b2_ready = 0;
 		cr_rs_pr_b2 = 0;
 
-		@(posedge clock)//#5
+		@(negedge clock)//#5
 	
 		rob_dispatch_num = 0;
 		fl_pr0 = 0;
@@ -256,10 +257,10 @@ module mt_test;
 		cr_rs_pr_b2_ready = 0;
 		cr_rs_pr_b2 = 0;
 
-		@(posedge clock)//#15
-		@(posedge clock)
+		@(negedge clock)//#15
+		@(negedge clock)//#25
 		reset = 0;
-		@(posedge clock)
+		@(negedge clock)//#35
 
 		rob_dispatch_num = 0;
 		fl_pr0 = 5;
@@ -303,7 +304,7 @@ module mt_test;
 		cr_rs_pr_b2_ready = 0;
 		
 
-		@(posedge clock)//#25
+		@(negedge clock)//#45
 		//Normal two dispatch and no complete
 		//Note that the values here may be of future use
 		//r3 is now p32, r4 is now p33
@@ -348,7 +349,7 @@ module mt_test;
 		cr_rs_pr_b1_ready = 1;
 		cr_rs_pr_b2_ready = 1;
 		
-		@(posedge clock)//#35
+		@(negedge clock)//#55
 		//Normal one dispatch and no complete
 		//Note that the values here may be of future use
 		//r9 is now p34
@@ -396,7 +397,7 @@ module mt_test;
 		
 
 
-		@(posedge clock)//#45
+		@(negedge clock)//#65
 		//Two dispatch on previous register, no complete
 		// r3, r4 is tested
 		//Note that the values here may be of future use
@@ -432,8 +433,8 @@ module mt_test;
 		// The correct output values
 		cr_rob_p0told = 15;
 		cr_rob_p1told = 16;
-		cr_rs_pr_a1 = 3;
-		cr_rs_pr_b1 = 4;
+		cr_rs_pr_a1 = 32;
+		cr_rs_pr_b1 = 33;
 		cr_rs_pr_a2 = 5;
 		cr_rs_pr_b2 = 6;
 
@@ -442,7 +443,7 @@ module mt_test;
 		cr_rs_pr_b1_ready = 0;
 		cr_rs_pr_b2_ready = 1;
 		
-		@(posedge clock)//#55
+		@(negedge clock)//#75
 		//No dispatch on previous registers, two completes
 		// r3, r4 is complete
 		//Note that the values here may be of future use
@@ -464,15 +465,15 @@ module mt_test;
 		rob_ar_a2 = 5;
 		rob_ar_b2 = 6;
 
-		cdb_broadcast = 2;
+		cdb_broadcast = 4'b0011;
 		cdb_pr_tags[0] = 32;
 		cdb_pr_tags[1] = 33;
-		cdb_pr_tags[2] = 0;
-		cdb_pr_tags[3] = 0;
+		cdb_pr_tags[2] = 34;
+		cdb_pr_tags[3] = 35;
 		cdb_ar_tags[0] = 3;
 		cdb_ar_tags[1] = 4;
-		cdb_ar_tags[2] = 0;
-		cdb_ar_tags[3] = 0;
+		cdb_ar_tags[2] = 9;
+		cdb_ar_tags[3] = 10;
 
 		// The correct output values
 		cr_rob_p0told = 36;
@@ -487,7 +488,7 @@ module mt_test;
 		cr_rs_pr_b1_ready = 0;
 		cr_rs_pr_b2_ready = 1;
 
-		@(posedge clock)//#65
+		@(negedge clock)//#85
 		begin
 		//Dispatch two on the registers just completed, no complete
 		// r3, r4 has been complete
@@ -535,7 +536,7 @@ module mt_test;
 		cr_rs_pr_b1_ready = 1;
 		cr_rs_pr_b2_ready = 1;
 		end
-		@(posedge clock)//#65
+		@(negedge clock)//#95
 		begin
 		//Dispatch two same registers
 		//Note that the values here may be of future use
@@ -569,8 +570,8 @@ module mt_test;
 		cdb_ar_tags[3] = 0;
 
 		// The correct output values
-		cr_rob_p0told = 38;
-		cr_rob_p1told = 38;
+		cr_rob_p0told = 19;
+		cr_rob_p1told = 40;
 
 		cr_rs_pr_a1 = 20;
 		cr_rs_pr_b1 = 23;
@@ -582,10 +583,11 @@ module mt_test;
 		cr_rs_pr_b1_ready = 1;
 		cr_rs_pr_b2_ready = 1;
 		end
-		@(posedge clock)//#75
+		@(negedge clock)//#105
 		begin
 		//Dispatch two same registers
 		//One of the operands is the same with the same dest
+		//completes two registers on other lines
 		//Note that the values here may be of future use
 		//r19 is now p40
 		rob_dispatch_num = 2;
@@ -606,6 +608,55 @@ module mt_test;
 		rob_ar_a2 = 23;
 		rob_ar_b2 = 24;
 
+		cdb_broadcast = 4'b1100;
+		cdb_pr_tags[0] = 0;
+		cdb_pr_tags[1] = 0;
+		cdb_pr_tags[2] = 38;
+		cdb_pr_tags[3] = 39;
+		cdb_ar_tags[0] = 0;
+		cdb_ar_tags[1] = 0;
+		cdb_ar_tags[2] = 17;
+		cdb_ar_tags[3] = 18;
+
+		// The correct output values
+		cr_rob_p0told = 20;
+		cr_rob_p1told = 41;
+
+		cr_rs_pr_a1 = 21;
+		cr_rs_pr_b1 = 41;
+		cr_rs_pr_a2 = 23;
+		cr_rs_pr_b2 = 24;
+
+		cr_rs_pr_a1_ready = 1;
+		cr_rs_pr_a2_ready = 1;
+		cr_rs_pr_b1_ready = 0;
+		cr_rs_pr_b2_ready = 1;
+		end		
+
+		@(negedge clock)//#115
+		begin
+		//Dispatch two same registers
+		//One of the operands is the same with the same dest
+		//Note that the values here may be of future use
+		//r19 is now p40
+		rob_dispatch_num = 2;
+		fl_pr0 = 43;
+		fl_pr1 = 44;
+
+		rob_ar_a_valid = 1;
+		rob_ar_b_valid = 1;
+		rob_ar_a1_valid = 1;
+		rob_ar_b1_valid = 1;
+		rob_ar_a2_valid = 1;
+		rob_ar_b2_valid = 1;
+
+		rob_ar_a = 20;
+		rob_ar_b = 28;
+		rob_ar_a1 = 17;
+		rob_ar_b1 = 18;
+		rob_ar_a2 = 23;
+		rob_ar_b2 = 24;
+
 		cdb_broadcast = 0;
 		cdb_pr_tags[0] = 5;
 		cdb_pr_tags[1] = 6;
@@ -617,13 +668,13 @@ module mt_test;
 		cdb_ar_tags[3] = 0;
 
 		// The correct output values
-		cr_rob_p0told = 41;
-		cr_rob_p1told = 41;
+		cr_rob_p0told = 42;
+		cr_rob_p1told = 28;
 
-		cr_rs_pr_a1 = 20;
-		cr_rs_pr_b1 = 41;
-		cr_rs_pr_a2 = 21;
-		cr_rs_pr_b2 = 22;
+		cr_rs_pr_a1 = 38;
+		cr_rs_pr_b1 = 39;
+		cr_rs_pr_a2 = 23;
+		cr_rs_pr_b2 = 24;
 
 		cr_rs_pr_a1_ready = 1;
 		cr_rs_pr_a2_ready = 1;
