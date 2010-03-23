b98e5d78c4a456606066ca4b074c270ec644c294
jeffxanthus <jeffxanthus@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-03-23 05:47:24 +0000

    new signals in fetch

--- verilog/if_mod.v
+++ verilog/if_mod.v
@@ -20,14 +20,17 @@ module if_mod(// Inputs
 				btb_pred_addr1,
 				Imem2proc_data,
 				Imem_valid,
-				id_inst_need_num, //new
+				id_dispatch_num,
 
 				// Outputs
-				id_NPC0,        // PC+4 of fetched instruction
-				id_NPC1,	//new
+				id_NPC,        // PC+4 of fetched instruction
 				id_IR0,         // fetched instruction out
 				id_IR1,		//  second fetched instruction out
 				proc2Imem_addr,
+				id_branch_taken0,
+				id_branch_taken1,
+				id_pred_addr0,
+				id_pred_addr1,
 				id_valid_inst0,
 				id_valid_inst1  // when low, instruction is garbage
 				);
@@ -41,16 +44,18 @@ input  [63:0] btb_pred_addr0;   // target pc: use if take_branch is TRUE
 input  [63:0] btb_pred_addr1;
 input  [63:0] Imem2proc_data;     // Data coming back from instruction-memory
 input  [1:0]  Imem_valid;
-input  [1:0]  busy;		//Whether RS and ROB are busy
+input  [1:0]  id_dispatch_num;		//Whether RS and ROB are busy
 
 output [63:0] proc2Imem_addr;     // Address sent to Instruction memory
-output [63:0] if_NPC0;         // PC of instruction after fetched (PC+4).
-output [63:0] if_NPC1;
-output [31:0] if_IR0;          // fetched instruction
-output [31:0] if_IR1;
-output        if_valid_inst0;
-output				if_valid_inst1;
-
+output [63:0] id_NPC;         // PC of instruction after fetched (PC+4).
+output [31:0] id_IR0;          // fetched instruction
+output [31:0] id_IR1;
+output        id_valid_inst0;
+output				id_valid_inst1;
+output				id_branch_taken0;
+output				id_branch_taken1;
+output				id_pred_addr0;
+output				id_pred_addr1;
 reg    [63:0] PC_reg;               // PC we are currently fetching
 
 wire   [63:0] PC_plus_4;
@@ -59,10 +64,10 @@ wire          PC_enable;
 wire          next_ready_for_valid;
 
 assign proc2Imem_addr = {PC_reg[63:3], 3'b0};
-
+assign [1:0] busy =  2'd2 - id_dispatch_num;
 // this mux is because the Imem gives us 64 bits not 32 bits
-assign if_IR0 = PC_reg[2] ? Imem2proc_data[63:32] : Imem2proc_data[31:0];
-assign if_IR1 = Imem2proc_data[31:0];
+assign id_IR0 = PC_reg[2] ? Imem2proc_data[63:32] : Imem2proc_data[31:0];
+assign id_IR1 = Imem2proc_data[31:0];
 // default next PC value
 assign PC_plus_4 = PC_reg + 4;
 assign PC_plus_8 = PC_reg + 8;
@@ -73,16 +78,22 @@ assign PC_plus_8 = PC_reg + 8;
 assign next_PC = busy[1] ? PC_reg: busy[0] ? PC_plus_4 : PC_plus_8;
 
 // The take-branch signal must override stalling (otherwise it may be lost)
-assign PC_enable0 = if_valid_inst0 ;//| bht_branch_taken0;
-assign PC_enable1 = if_valid_inst1 ;//| bht_branch_taken1;
+assign PC_enable0 = id_valid_inst0 ;//| bht_branch_taken0;
+assign PC_enable1 = id_valid_inst1 ;//| bht_branch_taken1;
     // Pass PC+4 down pipeline w/instruction
 	// I don't know what is going on here.......
-	
-assign if_NPC0 = busy[1] ? PC_reg : PC_plus_4;
-assign if_NPC1 = busy[1] ? PC_reg: busy[0] ? PC_plus_4 : PC_plus_8;
+assign id_NPC = busy[1] ? PC_reg : busy[0] ? PC_plus_4 : PC_plus_8;
 
-assign    if_valid_inst0 =  ~busy[1];
-assign	  if_valid_inst1 =  ~(busy[1]|busy[0]);	
+assign    id_valid_inst0 =  ~busy[1];
+assign	  id_valid_inst1 =  ~(busy[1]|busy[0]);	
+
+
+
+assign  id_branch_taken0 = 0;
+assign  id_branch_taken1 = 0;
+assign  id_pred_addr0 = 0;
+assign  if_pred_addr1 = 0;
+
 
 // This register holds the PC value
 always @(posedge clock)
