b3743379c5b2f93b5ff5f4a8879f321a27c747d0
yufu.fisher <yufu.fisher@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-03-23 01:37:32 +0000

    prf interface is finished

--- trunk/verilog/prf.v
+++ trunk/verilog/prf.v
@@ -18,6 +18,30 @@ module prf (// Inputs
 						rs_alu_mem_pra_idx0,
 						rs_alu_mem_pra_idx1,
 
+						alu_sim_wr_enable0,
+						alu_sim_pr_idx0,
+						alu_sim_pr_value0,
+
+						alu_sim_wr_enable1,
+						alu_sim_pr_idx1,
+						alu_sim_pr_value1,
+
+						alu_mul_wr_enable0,
+						alu_mul_pr_idx0,
+						alu_mul_pr_value0,
+
+						alu_mul_wr_enable1,
+						alu_mul_pr_idx1,
+						alu_mul_pr_value1,
+
+						alu_mem_wr_enable0,
+						alu_mem_pr_idx0,
+						alu_mem_pr_value0,
+
+						alu_mem_wr_enable1,
+						alu_mem_pr_idx1,
+						alu_mem_pr_value1,
+
 						// Outputs
 						alu_sim_pra_value0,
 						alu_sim_pra_value1,
