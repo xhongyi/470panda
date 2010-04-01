a22b85ddea32c36f2a8deeb23be6be3029947160
gohongyi <gohongyi@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-04-01 02:18:53 +0000

    btb modified and bht added

--- trunk/verilog/btb.v
+++ trunk/verilog/btb.v
@@ -10,9 +10,13 @@ module btb(//inputs
 					
 					if_pc,
 					
-					rob_mis_pred,
-					rob_mis_pred_pc,
-					rob_correct_npc,
+					rob_jump_retire0,
+					rob_jump_retire_pc0,
+					rob_correct_npc0,
+
+					rob_jump_retire1,
+					rob_jump_retire_pc1,
+					rob_correct_npc1,			
 					
 					//outputs
 					if_pred_addr0,
@@ -20,18 +24,24 @@ module btb(//inputs
 					);
 `ifndef NUM_BTB_ENTRIES
 `define	NUM_BTB_ENTRIES	1024
+`endif
+
 `ifndef	LOG_NUM_BTB_ENTRIES
 `define	LOG_NUM_BTB_ENTRIES	10
-`ifndef TAG_LENGTH
-`define	TAG_LENGTH	10
+`endif
 
 input	clock;
 input	reset;
 
 input	[63:0]					if_pc;//current PC
-input									rob_mis_pred;//1 if predicted wrong
-input	[63:0]					rob_mis_pred_pc;//if alu finds out the prediction is wrong, then need to fix the BTB for the new pre_pc for the same slot.
-input	[63:0]					rob_correct_npc;//the correct npc from alu
+
+input									rob_jump_retire0;//if 0 predicted wrong
+input	[63:0]					rob_jump_retire_pc0;
+input	[63:0]					rob_correct_npc0;
+
+input									rob_jump_retire1;//if 1 predicted wrong
+input	[63:0]					rob_jump_retire_pc1;
+input	[63:0]					rob_correct_npc1;
 
 output	[63:0]				if_pred_addr0;//the predictied npc
 output	[63:0]				if_pred_addr1;
@@ -41,9 +51,16 @@ reg	[`TAG_LENGTH-1:0]	next_tag				[`NUM_BTB_ENTRIES-1:0];
 reg	[18:0]						br_displ				[`NUM_BTB_ENTRIES-1:0];//branch displacement (I ignored the fact that branch ins may branch to a unaliased address)
 reg	[18:0]						next_br_displ		[`NUM_BTB_ENTRIES-1:0];
 
-wire if_pc_plus_four = if_pc + 3'd4;
+wire	[63:0]					if_pc_plus_four;
 
 integer i;
+
+wire	[63:0]				if_pred_addr0;
+wire	[63:0]				if_pred_addr1;
+
+assign if_pred_addr0 = {if_pc[63:`LOG_NUM_BTB_ENTRIES+2], br_displ[if_pc[`LOG_NUM_BTB_ENTRIES+1:2]], 2'b0};
+assign if_pred_addr1 = {if_pc_plus_four[`LOG_NUM_BTB_ENTRIES+2], br_displ[if_pc_plus_four[`LOG_NUM_BTB_ENTRIES+1:2]], 2'b0};
+assign if_pc_plus_four = if_pc + 3'd4;
 
 always @* begin
 	for(i=0; i<NUM_BTB_ENTRIES; i=i+1) begin
@@ -52,22 +69,30 @@ always @* begin
 	end
 
 //correct the BTB if there is a mispredicted branch.	
-	if (rob_mis_pred &&
-				(tag[rob_mis_pred_pc[`LOG_NUM_BTB_ENTRIES+1:2]] == rob_mis_pred_pc[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2]))
-		next_br_displ[rob_mis_pred_pc[`LOG_NUM_BTB_ENTRIES+1:2]] = 
-			rob_correct_npc[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2];
-	
+	if (rob_jump_retire0 && 
+			(tag[rob_jump_retire_pc0[`LOG_NUM_BTB_ENTRIES+1:2]] == 
+			rob_jump_retire_pc0[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2]))
+		next_br_displ[rob_jump_retire_pc0[`LOG_NUM_BTB_ENTRIES+1:2]] = rob_correct_npc0[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2];
+
+	if (rob_jump_retire1 && 
+			(tag[rob_jump_retire_pc1[`LOG_NUM_BTB_ENTRIES+1:2]] == 
+			rob_jump_retire_pc1[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2]))
+		next_br_displ[rob_jump_retire_pc1[`LOG_NUM_BTB_ENTRIES+1:2]] = rob_correct_npc1[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2];
+
+/*	
 //give out the predicted target npc
 //first by the first instruction is predicted as a branch
 	if (tag[if_pc[`LOG_NUM_BTB_ENTRIES+1:2]] == if_pc[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2])
 		if_pred_addr0 = {if_pc[63:`LOG_NUM_BTB_ENTRIES+2], br_displ[if_pc[`LOG_NUM_BTB_ENTRIES+1:2]], 2'b0};
+	else
 	if (tag[if_pc_plus_four[`LOG_NUM_BTB_ENTRIES+1:2]] == if_pc_plus_four[`TAG_LENGTH+`LOG_NUM_BTB_ENTRIES+1:`LOG_NUM_BTB_ENTRIES+2])
 		if_pred_addr1 = {if_pc_plus_four[`LOG_NUM_BTB_ENTRIES+2], br_displ[if_pc_plus_four[`LOG_NUM_BTB_ENTRIES+1:2]], 2'b0};
-	
+*/
+
 always @(posedge clock) begin
 	if (reset) begin
 		for (i=0; i<`NUM_BTB_ENTRIES; i=i+1) begin
-			tag[i] = 0;
+			tag[i] <= 0;
 			br_displ[i] <= 0;
 		end
 	end
