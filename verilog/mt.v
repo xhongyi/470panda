d29c351e79e51e2b206d68c7f0719aad1ea57b01
yufu.fisher <yufu.fisher@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-04-03 00:28:39 +0000

    mt is changed to record check point

--- verilog/mt.v
+++ verilog/mt.v
@@ -42,6 +42,14 @@ module mt (// Inputs
 					 cdb_ar_tag4,
 					 cdb_ar_tag5,
 
+					// For exception
+					 recover,
+					 rob_retire_num,
+					 rob_retire_ar0,
+					 rob_retire_ar1,
+					 rob_retire_pr0,
+					 rob_retire_pr1,
+
 					 //Outputs
 					 rob_p0told,
 					 rob_p1told,
@@ -97,6 +105,12 @@ module mt (// Inputs
 	input	[4:0]	cdb_ar_tag4;
 	input	[4:0]	cdb_ar_tag5;
 
+	input				recover;
+	input	[1:0]	rob_retire_num;
+	input	[4:0]	rob_retire_ar0;
+	input	[4:0]	rob_retire_ar1;
+	input	[6:0]	rob_retire_pr0;
+	input	[6:0]	rob_retire_pr1;
 
 	output	[6:0]	rob_p0told;
 	output	[6:0]	rob_p1told;
@@ -112,6 +126,8 @@ module mt (// Inputs
 
 	reg		[6:0]	pr_tags[31:0];
 	reg		[31:0]	ready_bits;
+
+	reg		[6:0]	pr_tags_retired[31:0];
 
 	reg		[31:0]	next_ready_bits;
 	wire 	rob_ar_a_valid = id_valid_inst0;
@@ -171,18 +187,20 @@ module mt (// Inputs
 	assign rs_pr_b2_ready = (rob_ar_a_valid && id_rb_idx1 == id_dest_idx0)? 1'b0: 
 																															 ready_bits[id_rb_idx1];
 
-	genvar i;
+/*	genvar i;
 	generate
 	for (i = 0; i < 32; i = i+1)
 	begin : foo
 		wire [6:0]	tmp_pr_tag = pr_tags[i];
 	end
-	endgenerate
+	endgenerate*/
+
+	integer i;
 
 	always @(posedge clock)
 	begin
 		if (reset) begin
-			pr_tags[0] <= `SD 7'd0;
+			/*pr_tags[0] <= `SD 7'd0;
 			pr_tags[1] <= `SD 7'd1;
 			pr_tags[2] <= `SD 7'd2;
 			pr_tags[3] <= `SD 7'd3;
@@ -206,21 +224,45 @@ module mt (// Inputs
 			pr_tags[21] <= `SD 7'd21;
 			pr_tags[22] <= `SD 7'd22;
 			pr_tags[23] <= `SD 7'd23;
-			pr_tags[24] <= `SD 7'd24;
+			pr_tags[24] <= `SD 7'd24;	
 			pr_tags[25] <= `SD 7'd25;
 			pr_tags[26] <= `SD 7'd26;
 			pr_tags[27] <= `SD 7'd27;
 			pr_tags[28] <= `SD 7'd28;
 			pr_tags[29] <= `SD 7'd29;
 			pr_tags[30] <= `SD 7'd30;
-			pr_tags[31] <= `SD 7'd31;
+			pr_tags[31] <= `SD 7'd31;*/
+
+			for (i = 0; i < 32; i= i+1)
+			begin
+				pr_tags[i] <= `SD i;
+			end
+
+			for (i = 0; i < 32; i=i+1)
+			begin
+				pr_tags_retired[i] <= `SD i;
+			end
 
 			ready_bits <= `SD 32'hffffffff;
-		end else begin
+		end 
+		else if (recover)
+		begin
+			for (i = 0; i < 32; i=i+1)
+				pr_tags[i] = pr_tags_retired[i];
+		end
+		else
+		begin
 			ready_bits <= `SD next_ready_bits;
 			pr_tags[id_dest_idx0] <= `SD pr_tags_next0;
 			pr_tags[id_dest_idx1] <= `SD pr_tags_next1;
+
+			if (rob_retire_num[1] || rob_retire_num[0])
+				pr_tags_retired[rob_retire_ar0] <= `SD rob_retire_pr0;
+			if (rob_retire_num[1])
+				pr_tags_retired[rob_retire_ar1] <= `SD rob_retire_pr1;
+				
 		end
+
 	end
 
 endmodule
