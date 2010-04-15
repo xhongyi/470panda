b6b4e3ecd63e8721bc8832073737e6ea6a7cbadf
jeffxanthus <jeffxanthus@437142a0-1b80-11df-8372-0b5b75a1557e>
2010-04-15 02:16:38 +0000

    mostly working dcache is updated

---
+++ trunk/testbench/dcache_test.v
@@ -0,0 +1,196 @@
+module dcache_test;
+`define VERILOG_CLOCK_PERIOD 10
+
+reg         	clock;
+reg         	reset;
+reg   [3:0] 	Dmem2proc_response;//number called
+reg  [63:0] 	Dmem2proc_data;
+reg   [3:0] 	Dmem2proc_tag;//get number
+reg 					rob_halt;
+reg  [63:0] 	proc2Dcache_addr;//addr on ld/st
+reg  [63:0]   proc2Dcache_st_addr;
+reg  [63:0]   proc2Dcache_st_data;
+wire  [63:0] 	cachemem_data;//data from cache
+wire         	cachemem_valid;//if the data is valid
+reg						rob_wr_mem;//lsq signal of store
+reg						lsq_rd_mem;//lsq signal of read
+reg		 [6:0]	lsq_pr;
+reg		 [4:0]	lsq_ar;
+wire					dcache_wr_dirty;//read the dirty bit of the 
+wire [63:0] 		proc2Dmem_data;
+wire  [1:0] 	proc2Dmem_command;//store? load? none?
+wire [63:0] 	proc2Dmem_addr;//addr to dmem
+wire					lsq_load_avail;
+wire [63:0] 	Dcache_data_out;     // value is memory[proc2Dcache_addr]
+wire        	Dcache_valid_out;    // when this is high
+wire 					cdb_load_en;
+wire  [6:0]   cdb_pr;
+wire  [4:0]   cdb_ar;
+wire [63:0]   dcache_wr_data;
+wire  [6:0] 	dcache_rd_idx;
+wire [21:0] 	dcache_rd_tag;
+wire  [6:0] 	dcache_wr_idx0;
+wire [21:0] 	dcache_wr_tag0;
+wire  [6:0] 	dcache_wr_idx1;
+wire [21:0] 	dcache_wr_tag1;
+wire        	dcache_wr_en0;
+wire					dcache_wr_en1;
+
+reg 			  correct;
+
+dcache dcache0(// inputs
+              clock,
+              reset,
+              
+              Dmem2proc_response,
+              Dmem2proc_data,
+              Dmem2proc_tag,
+							rob_halt,
+              proc2Dcache_addr,
+              proc2Dcache_st_data,
+              proc2Dcache_st_addr,
+              cachemem_data,
+              cachemem_valid,
+							dcache_wr_dirty,
+							rob_wr_mem,
+							lsq_rd_mem,
+							lsq_pr,
+							lsq_ar,
+              // outputs
+              proc2Dmem_command,
+              proc2Dmem_addr,
+              proc2Dmem_data,
+							lsq_load_avail,
+              Dcache_data_out,
+              Dcache_valid_out,   
+							cdb_load_en,
+							cdb_pr,
+							cdb_ar,
+							dcache_wr_data,
+              dcache_rd_idx,
+              dcache_rd_tag,
+              dcache_wr_idx1,
+              dcache_wr_tag1,
+							dcache_wr_idx0,
+							dcache_wr_tag0,
+              dcache_wr_en1,
+							dcache_wr_en0
+             );
+
+
+
+cachemem128x64 cache0(// inputs
+                       .clock(clock),
+                       .reset(reset), 
+                       .wr1_en(dcache_wr_en1),
+                       .wr1_tag(dcache_wr_tag1),
+                       .wr1_idx(dcache_wr_idx1),
+                       .wr1_data(dcache_wr_data),
+											 .wr0_en(dcache_wr_en0),
+											 .wr0_tag(dcache_wr_tag0),
+                       .wr0_idx(dcache_wr_idx0),
+                       .wr0_data(Dmem2proc_data),
+                       .rd1_tag(dcache_rd_tag),
+                       .rd1_idx(dcache_rd_idx),
+
+                       // outputs
+                       .rd1_data(cachemem_data),
+                       .rd1_valid(cachemem_valid),
+                       .wr1_dirty(dcache_wr_dirty)
+                      
+                      );
+
+	always
+	begin
+		#(`VERILOG_CLOCK_PERIOD/2.0);
+		clock = ~clock;
+	end
+
+	// Compare the results with the correct ones
+	always @(posedge clock)
+	begin
+		$monitor();
+
+
+		#(`VERILOG_CLOCK_PERIOD/4.0);
+
+		
+
+		correct = 1;
+end
+
+
+	initial
+	begin
+		clock = 0;
+
+		// Initializing
+
+		// The reg values
+		reset = 1;
+
+		clock = 0;
+		Dmem2proc_data			= 0;
+    Dmem2proc_response	= 0;
+		Dmem2proc_tag				= 0;
+    proc2Dcache_addr		= 0;
+    proc2Dcache_st_addr = 0;
+    proc2Dcache_st_data	= 0;
+		rob_halt						= 0;
+		rob_wr_mem					= 0;
+		lsq_rd_mem					= 0;
+		@(negedge clock)//#10
+		reset = 0;
+		@(negedge clock)//#20
+	  //Testcase 1: Save
+		proc2Dcache_st_addr		= 'h12340;
+    proc2Dcache_st_data		= 'h5678;
+		rob_wr_mem					= 1;
+		lsq_rd_mem					= 0;
+		lsq_pr							= 0;
+		lsq_ar              = 0;
+		Dmem2proc_response  = 2;
+		@(negedge clock)//#30
+		rob_wr_mem					= 0;
+	 // Dmem2proc_response  = 2;
+	 Dmem2proc_response	= 0;
+		@(negedge clock)
+		
+		
+		@(negedge clock)//#40
+		//Dmem2proc_data 			= 'h760;
+		Dmem2proc_tag 			= 'h2;
+		//Testcase 2: Load
+		
+    proc2Dcache_addr		= 'h12371;
+    proc2Dcache_st_data	= 'h5679;
+   
+		rob_wr_mem					= 0;
+		lsq_rd_mem					= 1;
+		Dmem2proc_response  = 1;
+		@ (negedge clock)//#50
+		lsq_rd_mem 					= 0;
+			Dmem2proc_response	= 0;
+		//Dmem2proc_response  = 1;
+		@(negedge clock)//#60
+	
+		
+		@(negedge clock)//#70
+		Dmem2proc_data 			= 'h890;
+		Dmem2proc_tag 			= 'h1;
+		//Testcase 2: Continuous Save
+		Dmem2proc_tag				= 1;
+		Dmem2proc_data			= 0;
+    proc2Dcache_addr		= 'h12340;
+    proc2Dcache_st_data	= 'h5680;
+ 
+		rob_wr_mem					= 0;
+		lsq_rd_mem					= 1;
+		@(negedge clock)//#80
+		Dmem2proc_tag				= 0;
+		@(negedge clock)
+		$finish;
+	end
+
+endmodule		
+						 
--- trunk/verilog/cachemem.v
+++ trunk/verilog/cachemem.v
@@ -5,43 +5,69 @@ module cachemem128x64 (// inputs
                        wr1_tag,
                        wr1_idx,
                        wr1_data,
+											 wr0_en,
+											 wr0_tag,
+											 wr0_idx,
+											 wr0_data,
                        rd1_tag,
                        rd1_idx,
 
                        // outputs
                        rd1_data,
-                       rd1_valid
+                       rd1_valid,
+                       wr1_dirty
+                       
                       );
 
-input clock, reset, wr1_en;
-input [6:0] wr1_idx, rd1_idx;
-input [21:0] wr1_tag, rd1_tag;
-input [63:0] wr1_data; 
+input clock, reset, wr1_en, wr0_en;
+input [6:0] wr1_idx, wr0_idx, rd1_idx;
+input [21:0] wr1_tag, wr0_tag, rd1_tag;
+input [63:0] wr1_data, wr0_data; 
 output [63:0] rd1_data;
 output rd1_valid;
-
+output wr1_dirty;
 reg [63:0] data [127:0];
 reg [21:0] tags [127:0]; 
 reg [127:0] valids;
-
+reg [127:0] dirty;
 
 assign rd1_data = data[rd1_idx];
 assign rd1_valid = valids[rd1_idx]&&(tags[rd1_idx] == rd1_tag);
+assign wr1_dirty = dirty[wr1_idx];
+assign mem_wr_en = dirty[wr1_idx] & valids[wr1_idx] & (wr1_tag != tags[wr1_idx]);
+assign mem_addr =  {tags[wr1_idx], wr1_idx};
+assign mem_data =  data[wr1_idx];
 
 always @(posedge clock)
 begin
-  if(reset) valids <= `SD 128'b0;
-  else if(wr1_en) 
+  if(reset) 
+	valids <= `SD 128'b0;
+  else 
+	begin
+		if(wr1_en) 
     valids[wr1_idx] <= `SD 1;
+		if(wr0_en)
+		valids[wr0_idx] <= `SD 1;
+	end
 end
 
 always @(posedge clock)
 begin
-  if(wr1_en)
-  begin
-    data[wr1_idx] <= `SD wr1_data;
-    tags[wr1_idx] <= `SD wr1_tag;
-  end
+	if(reset) dirty <= `SD 128'b0;
+	begin
+		if(wr1_en)
+		begin
+			data[wr1_idx] <= `SD wr1_data;
+			tags[wr1_idx] <= `SD wr1_tag;
+			dirty[wr1_idx] <= `SD ~dirty[wr1_idx];
+		end
+		if(wr0_en)
+		begin
+			data[wr0_idx] <= `SD wr0_data;
+			tags[wr0_idx] <= `SD wr0_tag;
+			dirty[wr0_idx] <= `SD ~dirty[wr0_idx];
+		end
+	end
 end
 
 endmodule
